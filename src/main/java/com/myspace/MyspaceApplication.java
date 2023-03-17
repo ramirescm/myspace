56e4b39bbb17e5f8f3f4725ff78265d0f87a23a1
ramires <ramirescm@gmail.com>
2023-03-17 08:15:48 -0300

    Startup project

---
+++ src/main/java/com/myspace/MyspaceApplication.java
@@ -0,0 +1,13 @@
+package com.myspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspaceApplication.class, args);
+	}
+
+}
