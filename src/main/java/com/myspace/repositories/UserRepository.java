8887293a36f3d12dd7706d07582e31a432e446f5
ramires <ramirescm@gmail.com>
2023-03-17 08:54:24 -0300

    user repository and service

---
+++ src/main/java/com/myspace/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.myspace.repositories;
+
+import com.myspace.entities.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
--- src/main/java/com/myspace/resources/UserResource.java
+++ src/main/java/com/myspace/resources/UserResource.java
@@ -1,23 +1,25 @@
 package com.myspace.resources;
 
 import com.myspace.entities.User;
+import com.myspace.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
     public ResponseEntity<List<User>> findAll(){
-        var list = new ArrayList<User>();
-        list.add(new User("1", "teste", "teste"));
-        list.add(new User("2", "vvv", "dddd"));
+       var list = userService.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
